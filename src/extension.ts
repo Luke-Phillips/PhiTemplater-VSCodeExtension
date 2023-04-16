e094915c1f36efbde8ffbd5896bbb4120b33af14
Luke Phillips <lphillips@cofisoftware.com>
2023-04-15 18:24:36 -0600

    add code to read squirt config file

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,85 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { TextDecoder } from "node:util";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log("Congratulations, you are squirting!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "squirt" is now active!');
+  interface SquirtConfigTemplate {
+    templateLocation: string;
+    templateName: string;
+    identifierPrefix?: string;
+    identifierSuffix?: string;
+  }
+  interface SquirtConfig {
+    templateDirectory: string;
+    defaultIdentifierPrefix: string;
+    defaultIdentifierSuffix: string;
+    templates: Array<SquirtConfigTemplate>;
+  }
+  const getConfig = async () => {
+    const configFile = (
+      await vscode.workspace.findFiles("squirt.config.json")
+    )[0];
+    const configBytes = await vscode.workspace.fs.readFile(configFile);
+    const configRaw = new TextDecoder().decode(configBytes);
+    const config: SquirtConfig = JSON.parse(configRaw);
+    return config;
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('squirt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Squirt!');
-	});
+  const showTemplateChooser = () => {
+    // pass
+  };
 
-	context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand(
+    "squirt.squirt",
+    async () => {
+      const config = await getConfig();
+      const templateNames: string[] = config.templates.map(
+        (template) => template.templateName
+      );
+      const qpOptions = {
+        title: "Choose Squirt Template",
+        placeHolder: "search templates",
+      };
+      const templateName = await vscode.window.showQuickPick(
+        templateNames,
+        qpOptions
+      );
+      console.log("🚀 ~ template name:", templateName);
+      //   const directoryPath = __dirname; // todo came from args
+      //   const options = { extension: null, templatesPath: "test" }; // todo came from args
+      //   const templatePath = path.resolve(
+      //     __dirname,
+      //     "./internalTemplates/squirtConfig.json"
+      //   );
+      //   const destinationFileName =
+      //     ".squirtrc" + (options.extension ? ".json" : "");
+      //   const destinationPath = path.resolve(directoryPath, destinationFileName);
+
+      //   const templateFileContents = (await fs.readFile(templatePath)).toString();
+
+      //   const defaultTemplateValues = {
+      //     templatesPath: "./squirtTemplates",
+      //   };
+
+      //   const templateValues = merge(defaultTemplateValues, {
+      //     templatesPath: options.templatesPath,
+      //   });
+
+      //   const destinationFileContents = tmpl(
+      //     templateFileContents,
+      //     templateValues
+      //   );
+      //   console.log("destinationPath", destinationPath);
+      //   console.log("destinationFileContents", destinationFileContents);
+      //   // await fs.outputFile(destinationPath, destinationFileContents)
+      vscode.window.showInformationMessage(
+        "Chosen template is " + templateName
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
